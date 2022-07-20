8a99c07dff04183cd7ac035d89f818926da8c070
dollarkillerx <adapawang@gmail.com>
2022-07-20 22:03:50 +0800

    js server

--- go.mod
+++ go.mod
@@ -8,7 +8,6 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	github.com/stretchr/testify v1.8.0 // indirect
-	github.com/viert/go-lame v0.0.0-20201108052322-bb552596b11d // indirect
 	golang.org/x/net v0.0.0-20200923182212-328152dc79b1 // indirect
 	golang.org/x/text v0.3.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- test/t_test.go
+++ test/t_test.go
@@ -1,29 +1,26 @@
 package test
 
 import (
-	"bytes"
 	"github.com/dollarkillerx/Dictate-words/pkg/models"
 	"github.com/dollarkillerx/urllib"
-	"github.com/viert/go-lame"
-	"io/ioutil"
 	"log"
 	"os"
 	"testing"
 )
 
 func TestM2(t *testing.T) {
-	start, err := ioutil.ReadFile("../stats/start.mp3")
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	ting, err := ioutil.ReadFile("../stats/ting.mp3")
-	if err != nil {
-		log.Fatalln(err)
-	}
+	//start, err := ioutil.ReadFile("../stats/start.mp3")
+	//if err != nil {
+	//	log.Fatalln(err)
+	//}
+	//
+	//ting, err := ioutil.ReadFile("../stats/ting.mp3")
+	//if err != nil {
+	//	log.Fatalln(err)
+	//}
 
 	var resp models.TtsResp
-	err = urllib.Post("http://tts_api.mechat.live/google_tts").SetJsonObject(models.TtsModel{
+	err := urllib.Post("http://tts_api.mechat.live/google_tts").SetJsonObject(models.TtsModel{
 		Text: "こんにちは、世界。",
 		Lang: "ja",
 	}).FromJsonByCode(&resp, 200)
@@ -45,19 +42,6 @@ func TestM2(t *testing.T) {
 		log.Fatalln(err)
 	}
 	defer create.Close()
-
-	enc := lame.NewEncoder(create)
-	defer enc.Close()
-
-	r := bytes.NewReader(start)
-	r.WriteTo(enc)
-
-	for i := 0; i < 3; i++ {
-		r := bytes.NewReader(bt)
-		r.WriteTo(enc)
-
-		r = bytes.NewReader(ting)
-		r.WriteTo(enc)
-	}
+	create.Write(bt)
 
 }
