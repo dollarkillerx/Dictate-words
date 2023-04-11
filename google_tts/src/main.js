f06741b5ba62c8f4a50bc65995f37eb4b033eced
dollarkillerx <adapawang@gmail.com>
2023-04-11 19:38:55 +0900

    fix api

--- google_tts/src/main.js
+++ google_tts/src/main.js
@@ -2,6 +2,8 @@ import Koa from 'koa'
 import KoaRouter from 'koa-router'
 import bodyParser from 'koa-bodyparser'
 import * as googleTTS from 'google-tts-api';
+import speak from 'google-translate-api-x';
+import { writeFileSync } from 'fs';
 
 const app = new Koa();
 
@@ -36,12 +38,50 @@ router.post('/google_tts', async (ctx) => {
         host: 'https://translate.google.com',
     });
 
+    speak('gata', {to: 'es'})
+
 
     ctx.response.type = 'application/json';
     ctx.body = {
         "url": url
     };
 })
+
+router.post('/google_tts2', async (ctx) => {
+    let lang = ctx.request.body.lang;
+    let text = ctx.request.body.text;
+    if (lang === undefined || text === undefined) {
+        ctx.status = 400
+        ctx.body = "なに"
+        return
+    }
+
+    lang = lang.replace(/^\s*|\s*$/g,"");
+    text = text.replace(/^\s*|\s*$/g,"");
+    if (lang==="" ||text==="") {
+        ctx.status = 400
+        ctx.body = "なに"
+        return
+    }
+
+    if (lang === 'jp' ) {
+        lang = "ja"
+    }
+
+
+    // const res = await  speak(text, {to: lang})
+
+    const res = await speak('gata', {to: 'es'}); // => Base64 encoded mp3
+    // writeFileSync('cat.mp3', res, {encoding:'base64'}); // Saves the mp3 to file
+
+    // writeFileSync('cat.mp3', res, {encoding:'base64'});
+
+    ctx.response.type = 'application/json';
+    ctx.body = {
+        "base64": res
+    };
+})
+
 
 async function main() {
     app.use(router.routes());
